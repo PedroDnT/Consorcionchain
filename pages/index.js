b5473a01fb3db8c3434bcf284736cc214535d2b5
PedroDnT <pedrotodescan@gmail.com>
2024-07-12 23:52:36 -0300

    Added a new home page component for the Consorcio Manager application.

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Consorcio Manager</h1>
+      <p>This is the main page of your application.</p>
+    </div>
+  );
+}
